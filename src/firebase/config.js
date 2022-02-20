6e85a1bb75508dd6b79d9ac60be0b6b1e9deede9
emilien-wittchen <abizw97@gmail.com>
2022-02-20 16:39:04 +0100

    login page, firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAP5aTCTzaovditHU4ieCN9G-vStTgbUJM',
+  authDomain: 'mymoney-f9e4d.firebaseapp.com',
+  projectId: 'mymoney-f9e4d',
+  storageBucket: 'mymoney-f9e4d.appspot.com',
+  messagingSenderId: '1096396897931',
+  appId: '1:1096396897931:web:1afc6a9c609ae9f206de8e',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+
+export {projectFirestore};
