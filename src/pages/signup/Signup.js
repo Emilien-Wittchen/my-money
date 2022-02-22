721f3cba7c92ee59fae1d02dd72b5eb711708274
emilien-wittchen <abizw97@gmail.com>
2022-02-22 17:32:03 +0100

    creating authContext, dispatching login action

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,19 +1,23 @@
-import styles from './Signup.module.css';
 import {useState} from 'react';
+import {useSignup} from '../../hooks/useSignup';
+
+// styles
+import styles from './Signup.module.css';
 
 export default function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [displayName, setDisplayName] = useState('');
+  const {signup, isPending, error} = useSignup();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password, displayName);
+    signup(email, password, displayName);
   };
 
   return (
     <form onSubmit={handleSubmit} className={styles['signup-form']}>
-      <h2>Signup</h2>
+      <h2>sign up</h2>
       <label>
         <span>email:</span>
         <input
@@ -38,7 +42,13 @@ export default function Signup() {
           value={displayName}
         />
       </label>
-      <button className='btn'>Singup</button>
+      {!isPending && <button className='btn'>sign up</button>}
+      {isPending && (
+        <button className='btn' disabled>
+          loading
+        </button>
+      )}
+      {error && <p>{error}</p>}
     </form>
   );
 }
